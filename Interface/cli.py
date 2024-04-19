4ce1a7e084a2603748a36c33efbb8ac8067da8ad
grapsgoober <twinklierre@gmail.com>
2024-04-19 14:14:42 +0700

    updated cli interface

--- Interface/cli.py
+++ Interface/cli.py
@@ -3,8 +3,8 @@ import argparse
 def get_args() -> tuple[str]:
     parser = argparse.ArgumentParser()
     parser.add_argument("mode")
-    parser.add_argument("ip")
-    parser.add_argument("port")
+    parser.add_argument("--ip")
+    parser.add_argument("--port")
     parser.add_argument("--keysize")
 
     args = parser.parse_args()
@@ -13,8 +13,14 @@ def get_args() -> tuple[str]:
     if args.mode not in supported_modes:
         raise Exception(f'Mode "{args.mode}" not allowed; only support {supported_modes}')
     
-    try: args.port = int(args.port)
-    except ValueError: raise Exception('Invalid port number')
+
+    communicate_modes = ['send', 'receive']
+    if args.mode in communicate_modes:
+        if args.ip == None or args.port == None:
+            raise Exception(f'ip and port required for {communicate_modes} modes')
+        
+        try: args.port = int(args.port)
+        except ValueError: raise Exception('Invalid port number')
     
     if args.keysize != None:
         try: args.keysize = int(args.keysize)
