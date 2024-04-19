7bbb66f91ca8fe79b7cfb9f8d19ded944d098ff9
grapsgoober <twinklierre@gmail.com>
2024-04-19 13:42:57 +0700

    implemented sender and receiver classes

---
+++ Chatroom/receiver.py
@@ -0,0 +1,30 @@
+import socket, RSA
+
+class ChatroomReceiver:
+    # initialize an encrypted TCP chatroom receiver
+    def __init__(self, ip: str, port: int, key: tuple[int, int]) -> None:
+        self.host = (ip, port)
+        self.key = key
+
+    # creates a receiver server, waits for client connection, and exchange keys
+    def __enter__(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(self.host)
+        sock.listen()
+        self.conn, self.addr = sock.accept()
+        _, modulus = self.key
+        self.conn.sendall(modulus.to_bytes(modulus.bit_length()))
+        return self
+    
+    # once the receiver is exited, close connection
+    def __exit__(self):
+        self.conn.close()
+
+    # try getting some data, if so decrypts
+    def get_message(self) -> str:
+        try: data = self.conn.recv(1024)
+        except ConnectionError: return None
+        if not data:            return None
+        return RSA.decrypt(data, self.key)
+
+
---
+++ Chatroom/sender.py
@@ -0,0 +1,23 @@
+import socket, RSA
+
+class ChatroomSender:
+    # initialize an encrypted chatroom sender
+    def __init__(self, ip: str, port: int, e: int = 65537) -> None:
+        self.dest = (ip, port)
+
+    # connects to a chatroom receiver, receive key exchange, and awaits send
+    def __enter__(self):
+        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.conn.connect(self.dest)
+        modulus = int.from_bytes(self.conn.recv(1024))
+        self.key = (65537, modulus)
+        return self
+    
+    # once the receiver is exited, close connection
+    def __exit__(self):
+        self.conn.close()
+
+    # encrypts and sends a message
+    def send_message(self, message: str):
+        cipher_text = RSA.encrypt(message, self.key)
+        self.conn.sendall(cipher_text)
