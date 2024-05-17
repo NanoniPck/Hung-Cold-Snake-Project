e5a03129774bbb44c6a51e888f47c0f5aaae4f1c
Graps <twinklierre@gmail.com>
2024-05-17 18:07:12 +0700

    (unstable) added --name argument for send mode and re-key mode

--- Interface/cli.py
+++ Interface/cli.py
@@ -6,6 +6,7 @@ def get_args() -> tuple[str]:
     parser.add_argument("--ip")
     parser.add_argument("--port")
     parser.add_argument("--keysize")
+    parser.add_argument("--name")
 
     args = parser.parse_args()
 
@@ -13,17 +14,20 @@ def get_args() -> tuple[str]:
     if args.mode not in supported_modes:
         raise Exception(f'Mode "{args.mode}" not allowed; only support {supported_modes}')
     
-
     communicate_modes = ['send', 'receive']
     if args.mode in communicate_modes:
         if args.ip == None or args.port == None:
             raise Exception(f'ip and port required for {communicate_modes} modes')
-        
         try: args.port = int(args.port)
         except ValueError: raise Exception('Invalid port number')
+
+    named_modes = ['send', 're-key']
+    if args.mode in named_modes and args.name == None:
+        raise Exception(f'recipient name is required for send mode')
+
     
     if args.keysize != None:
         try: args.keysize = int(args.keysize)
         except ValueError: raise Exception('Invalid keysize number')
 
-    return args.mode, args.ip, args.port, args.keysize
+    return args.mode, args.ip, args.port, args.keysize, args.name
