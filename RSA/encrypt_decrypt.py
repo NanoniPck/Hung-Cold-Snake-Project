ff2e7cff8fc686a1e782cd0ed155da8d2f8a547b
Graps <twinklierre@gmail.com>
2024-05-18 10:01:19 +0700

    implemented recursive block based encrypt/decrypt

--- RSA/encrypt_decrypt.py
+++ RSA/encrypt_decrypt.py
@@ -1,18 +1,30 @@
+from math import ceil
+
 
 # page. 4 RSA slide
 # encrypt C = M^e mod n 
 def encrypt(m : bytes, key : tuple[int, int]):
-    m = int.from_bytes(m)
     e, n = key
+    if len(m) > n.bit_length() // 8: 
+        m1, m2 = m[:len(m)//2], m[len(m)//2:]
+        c1 = encrypt(m1, key)
+        c2 = encrypt(m2, key)
+        return c1 + c2
+    
+    m = int.from_bytes(m)
     c: int = pow(m, e, n)
-    return c.to_bytes(c.bit_length() // 8 + 1)
+    return c.to_bytes(n.bit_length() // 8 + 1)
 
 # decrpyt M = C^d mod n
 def decrypt(c : bytes, key : tuple[int, int]):
     d, n = key
+
+    if len(c) > n.bit_length() // 8 + 1: 
+        c1, c2 = c[:len(c)//2], c[len(c)//2:]
+        m1 = decrypt(c1, key)
+        m2 = decrypt(c2, key)
+        return m1 + m2
+    
     c = int.from_bytes(c)
     m: int = pow(c, d, n)
-    return m.to_bytes(m.bit_length() // 8 + 1)
-
-
-
+    return m.to_bytes(ceil(m.bit_length() / 8))
