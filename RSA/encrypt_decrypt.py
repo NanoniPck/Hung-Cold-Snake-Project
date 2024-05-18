5c75ca8c9f6a85e42a6dc3a8639a16e898b50629
Graps <twinklierre@gmail.com>
2024-05-18 10:36:21 +0700

    added one-zeros padding

--- RSA/encrypt_decrypt.py
+++ RSA/encrypt_decrypt.py
@@ -5,29 +5,32 @@ from math import ceil
 # encrypt C = M^e mod n 
 def encrypt(m : bytes, key : tuple[int, int]):
     e, n = key
-    if len(m) > n.bit_length() // 8: 
+    block_size = n.bit_length() // 8
+    cipher_size = n.bit_length() // 8 + 1
+
+    if len(m) >= block_size: 
         m1, m2 = m[:len(m)//2], m[len(m)//2:]
         c1 = encrypt(m1, key)
         c2 = encrypt(m2, key)
         return c1 + c2
     
+    m += b'1' + b'0' * (block_size - len(m) - 1)
     m = int.from_bytes(m)
     c: int = pow(m, e, n)
-    return c.to_bytes(n.bit_length() // 8 + 1)
+    return c.to_bytes(cipher_size)
 
 # decrpyt M = C^d mod n
 def decrypt(c : bytes, key : tuple[int, int]):
     d, n = key
+    block_size = n.bit_length() // 8
+    cipher_size = n.bit_length() // 8 + 1
 
-    if len(c) > n.bit_length() // 8 + 1: 
+    if len(c) > cipher_size: 
         c1, c2 = c[:len(c)//2], c[len(c)//2:]
-        # TODO technically this is still wrong
-        # I should solve this problem using padding instead of 
-        # bytes-rounding
         m1 = decrypt(c1, key)
         m2 = decrypt(c2, key)
         return m1 + m2
     
     c = int.from_bytes(c)
     m: int = pow(c, d, n)
-    return m.to_bytes(ceil(m.bit_length() / 8))
+    return m.to_bytes(block_size).rsplit(b'1', 1)[0]
